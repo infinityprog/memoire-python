66ed59a65b18bb8e5974c1e74ff45240648b02e1
hugocianciolo <hugo220798@gmail.com>
2022-04-18 00:00:10 +0200

    check.py

---
+++ result/check.py
@@ -0,0 +1,39 @@
+import sys
+import csv
+
+def ajustTime(time):
+    if int(time[3:5]) + 1 >= 60:
+        time = '0' + str(int(time[1]) + 1) + time[2:5]
+    val = (int(time[3:5]) + 1) % 60
+    if val < 10:
+        return time[0:3] + '0' + str(val)
+    else:
+        return time[0:3] + str(val)
+
+if len(sys.argv) != 2:
+    print('Only one arg : Path')
+    quit()
+
+path = sys.argv[1]
+f = open(path)
+myReader = csv.reader(f, delimiter=';')
+oldTime = None
+for row in myReader:
+    time = row[0]
+    value = row[1]
+
+    if oldTime is None:
+        if time != '00:00':
+            print('Actual time: ' + time + ', expected time : 00:00')
+            quit()
+        oldTime = '00:00'
+    else:
+        oldTime = ajustTime(oldTime)
+        if oldTime != time:
+            print('Actual time: ' + time + ', expected time : ' + oldTime)
+            quit()
+print('File OK')
+
+
+
+
