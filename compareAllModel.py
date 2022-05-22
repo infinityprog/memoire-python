b3d36358a9939550b8d02c125d9441992f6639df
infinityprog <hugo220798@gmail.com>
2022-05-22 17:07:23 +0200

    graph

--- compareAllModel.py
+++ compareAllModel.py
@@ -4,7 +4,7 @@ import datetime
 
 from work.env import repCompare
 
-for model in ['yolov5l']:
+for model in ['yolov5n', 'yolov5s', 'yolov5m', 'yolov5l']:
 
     start = timeit.default_timer()
     os.system('python memoire.py '+ model + ' True')
@@ -14,4 +14,4 @@ for model in ['yolov5l']:
     f.write('Time: ' + str(datetime.timedelta(seconds=(stop - start))))
     f.close()
 
-    os.system('python buildResult.py ' + repCompare(model) + 'actual.csv result/expected-etroit.csv ' + repCompare(model) + 'result.txt')
+    os.system('python buildResult.py ' + repCompare(model) + 'actual.csv result/expected-large.csv ' + repCompare(model) + 'result.txt')
--- memoire.py
+++ memoire.py
@@ -34,7 +34,7 @@ def main():
     yoloModelName = sys.argv[1] if len(sys.argv) > 1 else 'yolov5n'
     isCompare = sys.argv[2] if len(sys.argv) > 1 else False
     yolo = Yolo(yoloModelName)
-    cap = cv2.VideoCapture('test.mp4')
+    cap = cv2.VideoCapture('large.mp4')
     # seconds = 0.1
     fps = cap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
     print('fps : ' + str(fps))
